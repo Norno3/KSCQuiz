a8db90dbc3c7428fc1888fd7fd9f6645476d988f
Joseph Banner <joseph.banner@hotmail.com>
2020-05-06 17:55:52 +0100

    connect to SQLDB in Azure

---
+++ Models/KSCQuiz.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quiz.Models
+{
+    public class KSCQuiz
+    {
+        public int ID { get; set; }
+        public string Category { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Quiz.Models;
 
 namespace Quiz
 {
@@ -24,6 +26,7 @@ namespace Quiz
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<MyDatabaseContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
